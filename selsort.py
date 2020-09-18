255b67a30592594572fd93948e0749a7f429ee93
ayush kedia <33809459+ayushkedia357@users.noreply.github.com>
2020-09-18 22:56:00 +0530

    Delete selsort.py

--- selsort.py
+++
@@ -1,9 +0,0 @@
-a=[int(i) for i in input().split(" ")]
-for i in range(len(a)):
-    for j in range(i+1,len(a)):
-        if a[i]>=a[j]:
-            temp=a[i]
-            a[i]=a[j]
-            a[j]=temp
-print(a)
-            
