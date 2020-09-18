b184f509b6d1179d927ce3dc449b2f6837293a7c
ayush kedia <33809459+ayushkedia357@users.noreply.github.com>
2020-09-18 22:51:42 +0530

    Delete bubsort.py

--- bubsort.py
+++
@@ -1,9 +0,0 @@
-a=[int(i) for i in input().split(" ")]
-for i in range(len(a)):
-    for j in range(len(a)-i-1):
-        b=a[j]
-        if b>=a[j+1]:
-            a[j]=a[j+1]
-            a[j+1]=b
-print(a)
-            
